6f9af584b63344bd8a43c40460a6eb69907ea93e
WOLF22008 <wildanfahrurrozi12@gmail.com>
2020-12-14 13:05:26 +0700

    first commit

---
+++ form_process.php
@@ -0,0 +1,22 @@
+<HTML>
+<HEAD>
+<TITLE>Tempat Pemrosesan</TITLE>
+</HEAD>
+<BODY BGCOLOR="lightgreen">
+<?
+echo "<b>Nama Anda:</b> $nama <br>\n";
+echo "<B>Jenis Kelamin:</B> $sex <BR>\n" ;
+echo "<B>Email Anda:</B> $email<BR>\n" ;
+echo "<B>Alamat Anda:</B> $alm<BR>\n" ;
+echo "<B>Pendidikan:</B> $sekolah<BR>\n" ;
+echo "<B>Pekerjaan:</B> $kerja<BR>\n" ;
+echo "<B>Umur:</B> $umur<BR>\n" ;
+echo "<B>Pengeluaran per bulan:</B> $keluar<BR>\n" ;
+echo "<h2>Terima kasih atas data yang dikirim pada kami</h2><BR>\n" ;
+?>
+<br>
+<p>Jika ingin mengirimkan data lain silahkan klik di bawah ini</p>
+<br>
+<a href="form_data.php">Formulir Data Diri</a>
+</BODY>
+</HTML>
